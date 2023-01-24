1251ec28f3b3f8d5fbf5ded92ecececd9405808c
Harish Kumar Natarajan <harishkr.coc@gmail.com>
2023-01-25 01:10:23 +0530

    Cleanup

--- harish-spring/src/main/java/SpringFramework/impl/CompScanSkill.java
+++ harish-spring/src/main/java/SpringFramework/impl/CompScanSkill.java
@@ -2,7 +2,6 @@ package SpringFramework.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
