1c038a254f94db60f370822b8b61e37f900ebc4e
Harish Kumar Natarajan <harishkr.coc@gmail.com>
2023-04-24 22:39:30 +0530

    More AspectJ

--- harish-spring/src/main/java/SpringFramework/impl/aop/AspectTestClass.java
+++ harish-spring/src/main/java/SpringFramework/impl/aop/AspectTestClass.java
@@ -1,5 +1,6 @@
 package SpringFramework.impl.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -7,11 +8,15 @@ import org.springframework.stereotype.Component;
 import SpringFramework.interfaces.Employee;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.annotation.Order;
 import org.springframework.context.ApplicationContext;
 import SpringFramework.impl.AnnotateEmployee;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
+@Order(1)
 public class AspectTestClass {
 
     @Pointcut("execution(public String getName())")
@@ -27,14 +32,25 @@ public class AspectTestClass {
         System.out.println("Before pc");
     }
 
+    @Before("execution(* set*(..))")
+    public void preSet(JoinPoint jp){
+        Object[] args = jp.getArgs();
+        System.out.println("Printing arguments from preSet");
+        Arrays.stream(args).forEach(System.out::println);
+    }
+
     public static void main(String[] args){
 
         ApplicationContext context = new AnnotationConfigApplicationContext(AnnotateEmployee.class);
         Employee emp = context.getBean("harishCompoScan",Employee.class);
 
+
+
         System.out.println(emp.getName());
 
         System.out.println(emp.getAge());
+
+        emp.setAge(69);
         
     }
 }
