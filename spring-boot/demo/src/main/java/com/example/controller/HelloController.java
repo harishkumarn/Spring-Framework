ac4db79ac32b81c5835b94cc5ec2cf4612f2e815
harish.kumar <harish.kumar@zohocorp.com>
2023-10-08 18:31:59 +0530

    unused imports

--- spring-boot/demo/src/main/java/com/example/controller/HelloController.java
+++ spring-boot/demo/src/main/java/com/example/controller/HelloController.java
@@ -7,8 +7,6 @@ import javax.validation.Valid;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
--- spring-boot/demo/src/main/java/com/example/handler/CustomExceptionHandler.java
+++ spring-boot/demo/src/main/java/com/example/handler/CustomExceptionHandler.java
@@ -4,7 +4,6 @@ package com.example.handler;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
-import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
